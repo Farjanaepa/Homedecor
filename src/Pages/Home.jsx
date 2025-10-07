4fb4b6e4e41ae0619eece68803110c527f8ca90c
Farjana Epa <farjanaepa@gmail.com>
2025-10-07 17:53:16 +0600

    navbar design processing

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { useLoaderData } from 'react-router';
 
 const Home = () => {
-    
+    const products = useLoaderData()
     return (
         <div>
-            <h1>Home Section</h1>
+         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+             {
+            products.map(product=>(
+                <h1>{product.name}</h1>
+            ))
+          }
+         </div>
         </div>
     );
 };
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,20 +4,27 @@ import Home from "../Pages/Home";
 import Products from "../Pages/Products";
 import MainLayout from "../Layouts/MainLayout";
 import ErrorPage from "../Pages/ErrorPage";
+import Wishlist from "../Pages/Wishlist";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
     errorElement: <ErrorPage></ErrorPage>,
+    hydrateFallbackElement: <p>Loading..</p>,
     children: [
  {
     index: true ,
-    element: <Home /> 
+    element: <Home /> ,
+    loader : () => fetch('./furnitureData.json')
   },
   {
     path: "/products",
     element: <Products />,
+  },
+   {
+    path: "/wishlist",
+    element: <Wishlist />,
   },
     ],
   },
