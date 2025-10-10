f7bb1cca4927388c752a735e7712172fb836869d
farjana epa <farjanaepa@gmail.com>
2025-10-11 00:58:17 +0600

    Wishlist add and product details

--- src/Pages/ProductDetails.jsx
+++ src/Pages/ProductDetails.jsx
@@ -5,22 +5,33 @@ import useProducts from '../hooks/useProducts';
 const ProductDetails = () => {
     const { id } = useParams()
      const { products, loading,error } = useProducts()
-   
-
+  
     const product = products.find(p => String(p.id) === id)
    
 
     if(loading) return <p> loading....</p>
-    const { name, image,category, price} = product
+    const { name, image,category, price} = product || {}
 
 
     const handleAddToWishlist = () =>{
 
-        const existingList= JSON.parse(localStorage.getItem('wishlist'))
-        console.log(existingList)
+      const existingList= JSON.parse(localStorage.getItem('wishlist'))
+      let updatedList = []
+      if (existingList) {
+       const isDuplicate = existingList.some(p => p.id === product.id)
+        if (isDuplicate) return alert ('sorry vai') 
+          updatedList = [...existingList, product]
+          
+        } else {
+          updatedList.push(product)
+        }
+        localStorage.setItem('wishlist' , JSON.stringify(updatedList))
 
-        localStorage.setItem('wishlist' , JSON.stringify(product))
-    }
+      }
+
+
+       
+    
     return (
         <div className="card bg-base-100 shadow-sm hover:scale-100 transition">
   <figure className='h-48 overflow-hidden'>
--- src/Pages/Wishlist.jsx
+++ src/Pages/Wishlist.jsx
@@ -1,10 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Wishlist = () => {
+    const[wishlist, setWishlist] = useState([])
+    const [sortOrder, setSortOrder] = useState('none')
+    useEffect (() => {
+        const savedList = JSON.parse(localStorage.getItem('wishlist'))
+        if (savedList) setWishlist(savedList) 
+    }, [] ) 
+
+
+    const sortedItem = () => {
+        if(sortOrder === 'price-asc') {
+            return [...wishlist].sort((a,b) => a.price -b.price)
+        } else if (sortOrder === 'price-desc') {
+            return [...wishlist].sort((a,b) => b.price - a.price)
+        } else {
+            return wishlist
+        }
+    }
     return (
-        <div>
-            <h1> Wish list page</h1>
+        <>
+        <div className='space-y-6'>
+         <div>
+               
+         <h1> Wish list page : {wishlist.length} Product found </h1>
+        
+         <label className='form-control w-full max-w-xs'>
+          <select
+            className='select select-bordered'
+            value={sortOrder}
+            onChange={e => setSortOrder(e.target.value)}
+          >
+            <option value='none'>Sort by price</option>
+            <option value='price-asc'>Low-&gt;High</option>
+            <option value='price-desc'>High-&gt;Low</option>
+          </select>
+        </label>
+         </div>
+
+            <div className='space-y-3'>
+                {sortedItem.map(p => (
+                    <div key={p.id} className="card card-side bg-base-100 shadow-sm">
+            <figure>
+            <img 
+           className='w-40 h-28 object-cover'
+           src={p.image} 
+           alt={p.name} />
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title">{p.name}</h2>
+                <p className='text-base-content/70'>{p.category}</p>
+                <div className="card-actions justify-end">
+                <button className="btn btn-primary">Remove</button>
+                </div>
+            </div>
+    </div>))}
+            
+            </div>
         </div>
+
+        </>
+        
+
     );
 };
 
